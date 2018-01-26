10864a21c8f1e9ae0a805ce4dfb96f7fa2dc8d9e
Alan Martins <alanhorcrux@gmail.com>
2018-01-26 19:26:43 -0200

    inserir e alterar

--- index.php
+++ index.php
@@ -25,8 +25,21 @@ echo $root;
 //echo json_encode($search);
 
 //Carrega um usuario usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("eu", "654321");
+//echo $usuario;
+
+//$aluno = new Usuario();
+//$aluno->setDeslogin("aluno");
+//$aluno->setDessenha("@lun0");
+//$aluno->insert();
+//echo $aluno;
+
 $usuario = new Usuario();
-$usuario->login("eu", "654321");
+
+$usuario->loadByid(8);
+
+$usuario->update("professor","!@#@%$%");
 
 echo $usuario;
 
